2916c0e4d2115f31f2687b34207f771da8eafb0d
Sayan Maity <jcsayan7@gmail.com>
2026-03-05 22:48:27 +0530

    supabase link reestablished

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // Supabase Configuration
-const supabaseUrl = 'https://ee-db-proxy.electricalevolution2-0.workers.dev';
+const supabaseUrl = 'https://mnkytgygusfxngrdnseg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ua3l0Z3lndXNmeG5ncmRuc2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NTg2MzAsImV4cCI6MjA4NzMzNDYzMH0.A_upRIOaFRCFVMPVX2VVXornCtG3pGVQ7qLYBwijIUs';
 
 // Global Security Utils
